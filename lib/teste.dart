3381ab85c05e2f89af51faec1139be3fdf2f9ae0
jhonathanqz029 <74057391+jhonathanqz029@users.noreply.github.com>
2021-04-09 13:30:42 -0300

    teste 2

---
+++ lib/teste.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class HomeCores extends StatelessWidget {
+  var back1 = Colors.green;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Teste de Cores'),
+        centerTitle: true,
+        backgroundColor: Color(0xfff1c426),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            paleta(
+              Color(0xff7a7a7a),
+            ),
+            paleta(
+              Color(0xff968c8e),
+            ),
+            paleta(
+              Color(0xff413d3e),
+            ),
+            paleta(
+              Color(0xff141313),
+            ),
+            paleta(
+              Color(0xfffdf3d0),
+            ),
+            paleta(
+              Color(0xfff6d76c),
+            ),
+            paleta(
+              Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget paleta(Color cor) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          height: 50,
+          color: cor,
+        ),
+      ),
+    );
+  }
+}
